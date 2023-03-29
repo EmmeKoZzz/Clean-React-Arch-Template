5b3ca7dc0f0a304d732eab3de9939bad5b1f6b9e
EmmeKoZzz <119811212+EmmeKoZzz@users.noreply.github.com>
2023-03-28 22:07:36 -0400

    more exampled structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,27 @@
+import CleanArchLogo from './assets/cleanArch.svg';
+import PascalCamelCase from './components/component-name';
+
 function App() {
-	return <h1>Clean Arch</h1>;
+	return (
+		<>
+			<div
+				style={{
+					display: 'flex',
+					height: '50px',
+					alignItems: 'center',
+					margin: '1rem',
+				}}
+			>
+				{/*
+			Replace inline styles, do not use it
+			and enjoy using a clean start proyect, yeah
+		*/}
+				<img src={CleanArchLogo} alt="logo" style={{ height: '50px' }} />
+				<h1>Clean Arch</h1>
+			</div>
+			<PascalCamelCase />
+		</>
+	);
 }
 
 export default App;
---
+++ src/components/component-name.tsx
@@ -0,0 +1,14 @@
+export default function PascalCamelCase() {
+	return (
+		<ul
+			style={{
+				paddingLeft: '1rem',
+				listStyle: 'none',
+			}}
+		>
+			<li>*use PascalCamelCase for nameing components</li>
+			<li>*use kebab-case for naming component files</li>
+			<li>*declare just one component by file</li>
+		</ul>
+	);
+}
